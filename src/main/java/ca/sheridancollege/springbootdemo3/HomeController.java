95cac55c6cc57f493424ad7d001d4706848b5332
Shankar Ghimire <ssghimire@gmail.com>
2020-10-06 00:14:55 -0400

    First commit

---
+++ src/main/java/ca/sheridancollege/springbootdemo3/HomeController.java
@@ -0,0 +1,12 @@
+package ca.sheridancollege.springbootdemo3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String goHome(){
+        return "home.html";
+    }
+}
---
+++ src/test/java/ca/sheridancollege/springbootdemo3/Springbootdemo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.springbootdemo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootdemo3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
